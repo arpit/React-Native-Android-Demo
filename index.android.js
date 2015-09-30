d2a1a5b312b1f7b24255be4545a3574e1cfd84d4
Arpit Mathur <mathur.arpit@gmail.com>
2015-09-30 16:45:13 -0400

    First working build for Android

---
+++ index.android.js
@@ -0,0 +1,88 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ToolbarAndroid,
+  TouchableOpacity,
+  ToastAndroid
+} = React;
+
+var ch2 = React.createClass({
+
+getInitialState: function() {
+    // naming it initialX clearly indicates that the only purpose
+    // of the passed down prop is to initialize something internally
+    return {count: 0};
+  },
+
+  render: function() {
+    return (
+    <View style={{flex: 1}}>
+      <ToolbarAndroid
+        style={styles.toolbar}
+        titleColor="#ffffff"
+        title='My Awesome App' />
+
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        
+        <TouchableOpacity onPress={this.handlePress}>
+        <View style={styles.button} >
+          <Text style={{color: 'black'}}>
+            Count {this.state.count}
+          </Text>
+        </View>
+      </TouchableOpacity>
+
+      </View>
+    );
+  },
+
+  handlePress: function(){
+    this.setState({count: this.state.count + 1});
+    ToastAndroid.show('Updating', ToastAndroid.SHORT)
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  toolbar: {
+    backgroundColor: '#FF6600',
+    height: 56,
+  },
+  button:{
+    backgroundColor: '#dfdfdf',
+    textAlign:'center',
+    marginTop:20,
+    alignSelf:'center',
+    width: 80,
+    borderRadius: 5,
+    padding:10,
+    alignItems: 'center',
+  }
+});
+AppRegistry.registerComponent('ch2', () => ch2);
